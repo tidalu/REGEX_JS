80b6134df35f90bd64e08ba5df93bcb62dbdc57f
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-22 15:38:53 +0100

    regex: match, split, test, replace, exec()

--- whatIsRegex.js
+++ whatIsRegex.js
@@ -44,29 +44,98 @@
 
 /\d{3}\s\w{5}/g; // '333    rooms'  or '333 rooms'
 
-
-
 // special characters
-
 
 // '+'  the one-or-more quantifier
 // '\'  the excape character
 // '[]'  the character set
 // '[^]' the negate symobol in a character set
-// '?'  the zero oro one quantifier (makes a preceeding character optional)  like 
-/hello?/g // these is o is optional hell, and hello are acceptable
+// '?'  the zero oro one quantifier (makes a preceeding character optional)  like
+/hello?/g; // these is o is optional hell, and hello are acceptable
 // '.'  aby character whatsoever (except the newline character )
-/hello./g
+
 // '*' zero or more quantifies (a bit like +)
-/a[a-z]*/ // 
 
+/^[a-z]{5}$/;
 
-    /^[a-z]{5}$/
+// ^ for the beginning of the line
+// $ fior the end of theline
+// \b fro word boundary
 
-// ^ for the beginning of the word
-// $ fior the end of the word
+/\b\w{4}\b/g; // which looks for only four length word
 
+/ (p|t)yer/i; // pyer fo tyer both are acceptable
+/ (pet|toy|crazy) ?rabbit/gi;
 
-    / (p | t)yer / i; // pyer fo tyer both are acceptable
-/ (pet|toy|crazy) ?rabbit/gi; 
-    
+// course II
+
+// sample
+// text 646-678-4545
+// 343.232.5555
+// (343)323-2323
+
+// phone number
+
+/\(?\d{3}[-.)]\d{3}[-.]\d{4}/;
+
+// capturing groups
+
+/\(?(\d{3})[-.)]\d{3}[-.]\d{4}/;
+
+// text
+
+/**
+ *
+ *  danies, hjiksjdskn
+ *  kshdskdn, sjdhsjkd
+ *  sjkdhskjdh, dksjdhksj
+ *
+ */
+
+/(\w+),\s(\w+)/;
+
+//  <a href='https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'>HTML5</a>
+//  <a href='https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'>CSS3</a>
+//  <a href='https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white'>C</a>
+
+/\[(.*?)\]\((http.*?)\)/;
+
+// okay let me tell about capturing groups, it is just to capture some part from whole
+// regex finds, like above examples, then how we can use is like in two options, $1 $2
+// like that numbers are grouped orders, it is mainly to us in replace mode, main part to
+// capture groups is just to put() brackets before and after the wanted groups, let's seee
+// more examples for the second type of calling captured groups
+
+/\b(\w+)\s\1\b/; // so here what we are doing is capturing firs one, and after space using that one    \1 \2 \3 this is the second way fo using captures , but it is within regex not outside
+var s = 'hey man How is it Going ?';
+var r = /^\d{3}$/;
+
+r.test('hello'); // false
+r.test('123'); // true
+r.test('123ABC'); // false
+
+// match
+r = /\b[a-z]+\b/gi;
+let res = s.match(r);
+
+// console.log(res);
+
+// to get the group in js , we should not put global flag, so, then we will get array with  elements using match function , first one is main result , second one is groups
+
+// let us talk about exec()  -- >  executes a search with this regular expression for a match in a specified string and returns a result array, or null .
+
+// so exec does what it, it is continius , till null,  like first call returns the first match array with captured groups, second time call return that for second match till there is not matches are left, then it restuns null
+
+r.exec(s);
+
+// split(--regex--) and replace(--regex--, (str, fn)) with regex
+
+s = 'unicorns and rainbows, and Cupcakes';
+
+res = s.split(/[,\s/]+/);
+console.log(res);
+
+// replace()
+
+s = s.replace(/([aeiou])/g, '$1$1');
+console.log(s);
